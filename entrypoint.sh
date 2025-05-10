8a0e6beb3cc55e4f201526eaabb1634245c06e54
Quang Tín <nlqtin@tma.com.vn>
2025-05-10 10:49:47 +0700

    Inititial github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Starting Rasa Action Server on port 5055..."
 exec rasa run actions &
 
-echo "Starting Rasa Server on port ${RASA_PORT:-5005} with API enabled..."
-exec rasa run -m rasa_model.tar.gz --enable-api --cors "*" --debug
+echo "Starting Rasa Server on port with API enabled..."
+exec rasa run -m models/rasa_model.tar.gz --enable-api --cors "*" --debug
